deb6614738d420ffc95d8dc73c0d51391aeabfcf
James Harris <wopian@wopian.me>
2017-07-18 07:36:37 +0100

    feat: switch to kitsu and use global updated value

--- cron/index.js
+++ cron/index.js
@@ -1,25 +1,12 @@
-import rp from 'request-promise'
+import Kitsu from 'kitsu'
 import low from 'lowdb'
 import stringify from 'json-stringify-pretty-compact'
 import fileAsync from 'lowdb/lib/storages/file-async'
 import wmean from 'weighted-mean'
 import { season, year } from '../src/util'
 
-const version = 'edge'
-const base = `https://kitsu.io/api/${version}`
-// Days since epoch
-// const now = ~~(Date.now() / 1000 / 60 / 60 / 24)
+const kitsu = new Kitsu()
 const now = new Date().toISOString()
-
-const q = {
-  // 20 results per request
-  limit: 'page[limit]=20',
-  // Currently airing tv and min userCount of 10
-  filter: 'filter[status]=current&filter[subtype]=tv',
-  // Sort by highest rated then most users
-  sort: 'sort=-averageRating,-userCount',
-  fields: 'fields[anime]=slug,canonicalTitle,ratingFrequencies,userCount,favoritesCount,posterImage'
-}
 
 // Load season database
 const db = low(`./data/${year()}-${season()}.json`, {
@@ -30,7 +17,7 @@ const db = low(`./data/${year()}-${season()}.json`, {
 })
 
 // Set defaults if new season
-db.defaults({ data: {} }).write()
+db.defaults({ data: {}, updated: '' }).write()
 
 // Array of all shows that have started airing. Airing shows will be
 // removed, leaving only shows that have finished - which will be
@@ -47,11 +34,42 @@ function display (count, processed, offset) {
   }
 }
 
-async function get (path, { offset = 0, id = null, upcoming = false } = {}) {
+async function get (model, { offset = 0, id = null, upcoming = false } = {}) {
   try {
-    if (id) return JSON.parse(await rp(`${base}/${path}/${id}?${q.fields},endDate`))
-    else if (upcoming) return JSON.parse(await rp(`${base}/${path}?page[offset]=${offset}&${q.limit}&${q.sort}&${q.fields}&filter[subtype]=tv&filter[season_year]=${year()}&filter[season]=${season()}`))
-    else return JSON.parse(await rp(`${base}/${path}?page[offset]=${offset}&${q.limit}&${q.filter}&${q.sort}&${q.fields}`))
+    if (id) return kitsu.get(`${model}/${id}`, {
+      fields: {
+        anime: 'slug,canonicalTitle,ratingFrequencies,userCount,favoritesCount,posterImage,endDate'
+      }
+    })
+    else if (upcoming) return kitsu.get(model, {
+      page: {
+        offset,
+        limit: 20
+      },
+      fields: {
+        anime: 'slug,canonicalTitle,ratingFrequencies,userCount,favoritesCount,posterImage'
+      },
+      filter: {
+        subtype: 'tv',
+        season_year: year(),
+        season: season()
+      },
+      sort: '-averageRating,-userCount'
+    })
+    else return kitsu.get(model, {
+      page: {
+        offset,
+        limit: 20
+      },
+      filter: {
+        status: 'current',
+        subtype: 'tv'
+      },
+      fields: {
+        anime: 'slug,canonicalTitle,ratingFrequencies,userCount,favoritesCount,posterImage'
+      },
+      sort: '-averageRating,-userCount'
+    })
   } catch (err) {
     console.error(err)
   }
@@ -69,46 +87,45 @@ function calcRatings (frequency) {
   }
 }
 
-async function set (id, attributes, { mean, usersRated }) {
+async function set (id, data, { mean, usersRated }) {
   try {
     db.set(`data.${id}`, {
       id,
-      updated: now,
       mean: [ mean ],
-      slug: attributes.slug,
+      slug: data.slug,
       usersRated: [ usersRated ],
-      users: [ attributes.userCount ],
-      title: attributes.canonicalTitle,
-      poster: attributes.posterImage.medium,
-      favorites: [ attributes.favoritesCount ]
+      users: [ data.userCount ],
+      title: data.canonicalTitle,
+      poster: data.posterImage.medium,
+      favorites: [ data.favoritesCount ]
     }).write()
   } catch (err) {
     console.error(err)
   }
 }
 
-async function update (id, attributes, { mean, usersRated }) {
+async function update (id, data, { mean, usersRated }) {
   try {
     const latest = db.get(`data.${id}`).value()
-    latest.updated = now
     latest.mean.push(mean)
-    latest.slug = attributes.slug
+    latest.slug = data.slug
     latest.usersRated.push(usersRated)
-    latest.users.push(attributes.userCount)
-    latest.title = attributes.canonicalTitle
-    latest.poster = attributes.posterImage.medium
-    latest.favorites.push(attributes.favoritesCount)
+    latest.users.push(data.userCount)
+    latest.title = data.canonicalTitle
+    latest.poster = data.posterImage.medium
+    latest.favorites.push(data.favoritesCount)
     db.set(`data.${id}`, latest).write()
   } catch (err) {
     console.error(err)
   }
 }
 
-async function check ({ id, attributes }) {
+async function check (data) {
   try {
-    const ratings = calcRatings(attributes.ratingFrequencies)
-    if (!db.get(`data.${id}`).value()) await set(id, attributes, ratings)
-    else await update(id, attributes, ratings)
+    const ratings = calcRatings(data.ratingFrequencies)
+    if (!db.get(`data.${data.id}`).value()) await set(data.id, data, ratings)
+    else await update(data.id, data, ratings)
+    db.set('updated', now).write()
   } catch (err) {
     console.error(err)
   }
@@ -148,7 +165,7 @@ async function addUpcoming (offset = 0) {
       const exists = db.get('data').find({ id: anime.id }).value()
       if (typeof exists === 'undefined') {
         await check(anime)
-        console.log(`  Added ${anime.attributes.canonicalTitle}`)
+        console.log(`  Added ${anime.canonicalTitle}`)
       }
     })
 
@@ -168,13 +185,13 @@ function getAired () {
       // Check if the show ended within this season
       // If not, it was an erroneous entry and shouldn't be in the season's data
       // at all - thus remove it entirely
-      const ended = data.attributes.endDate
+      const ended = data.endDate
       const cutoff = new Date(now).getTime() - (3 * 30 * 24 * 60 * 60 * 1000)
       const endDate = new Date(ended === null ? new Date(now).getTime() + (24 * 60 * 60 * 100) : ended).getTime()
       if (endDate - cutoff > 0) await check(data)
       else await remove(data)
 
-      console.log((endDate - cutoff > 0 ? 'Updated ' : 'Removed ') + data.attributes.canonicalTitle)
+      console.log((endDate - cutoff > 0 ? 'Updated ' : 'Removed ') + data.canonicalTitle)
     })
   } catch (err) {
     console.error(err)
