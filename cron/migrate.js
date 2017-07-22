101e2e4f8751318129ed3f379e4f470fa0c2dfaa
James Harris <wopian@wopian.me>
2017-07-22 20:35:23 +0100

    refactor: backfill date values

--- cron/migrate.js
+++ cron/migrate.js
@@ -17,35 +17,12 @@ readdir(dir, (err, files) => {
       const { data, updated } = JSON.parse(DATA)
 
       Object.keys(data).forEach(el => {
-        data[el].d = []
-        for (let i = 0; i < data[el].mean.length; i++) {
-          data[el].d.push({
-            i,
-            m: data[el].mean[i],
-            r: encode(data[el].usersRated[i]),
-            u: encode(data[el].users[i]),
-            f: encode(data[el].favorites[i])
-          })
+        let date = new Date(updated)
+
+        for (let i = data[el].d.slice(-1)[0].i; i >= 0; i--) {
+          data[el].d[i].d = encode(date.getTime())
+          date.setDate(date.getDate() - 1)
         }
-
-        delete data[el].mean
-        delete data[el].users
-        delete data[el].usersRated
-        delete data[el].favorites
-
-        /*
-        data[el].i = encode(~~data[el].id)
-        delete data[el].id
-
-        data[el].s = data[el].slug
-        delete data[el].slug
-
-        data[el].t = data[el].title
-        delete data[el].title
-
-        data[el].p = encode(~~data[el].poster.split`?`[1])
-        delete data[el].poster
-        */
       })
 
       writeFile(`${dir}/${file}`, stringify({ data, updated }, { maxLength: 250 }), err3 => {
