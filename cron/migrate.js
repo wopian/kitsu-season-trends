5740c5450803535c5e2c25311d4e664c34ca7e99
James Harris <wopian@wopian.me>
2017-07-22 20:21:49 +0100

    refactor: migrate data structure

--- cron/migrate.js
+++ cron/migrate.js
@@ -4,6 +4,7 @@
 
 const stringify = require('json-stringify-pretty-compact')
 const { readdir, readFile, writeFile } = require('fs')
+const { encode } = require('base-65503')
 
 const dir = './data'
 
@@ -13,11 +14,38 @@ readdir(dir, (err, files) => {
     readFile(`${dir}/${file}`, 'utf8', (err2, DATA) => {
       if (err2) throw err2
 
-      const { data } = JSON.parse(DATA)
-      const updated = data[Object.keys(data)[0]].updated
+      const { data, updated } = JSON.parse(DATA)
 
       Object.keys(data).forEach(el => {
-        delete data[el].updated
+        data[el].d = []
+        for (let i = 0; i < data[el].mean.length; i++) {
+          data[el].d.push({
+            i,
+            m: data[el].mean[i],
+            r: encode(data[el].usersRated[i]),
+            u: encode(data[el].users[i]),
+            f: encode(data[el].favorites[i])
+          })
+        }
+
+        delete data[el].mean
+        delete data[el].users
+        delete data[el].usersRated
+        delete data[el].favorites
+
+        /*
+        data[el].i = encode(~~data[el].id)
+        delete data[el].id
+
+        data[el].s = data[el].slug
+        delete data[el].slug
+
+        data[el].t = data[el].title
+        delete data[el].title
+
+        data[el].p = encode(~~data[el].poster.split`?`[1])
+        delete data[el].poster
+        */
       })
 
       writeFile(`${dir}/${file}`, stringify({ data, updated }, { maxLength: 250 }), err3 => {
