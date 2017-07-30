80fd5e5bb929d7e8e2afff38e110236dcb96bdb0
James Harris <wopian@wopian.me>
2017-07-30 18:44:00 +0100

    refactor: simplify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,17 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { season, year } from '../util'
+import { season as s, year as y } from '../util'
 
-function HeaderContent ({ match }) {
+export function Header () {
+  const { pathname } = new URL(window.location)
+  const [ , year, season ] = pathname.split`/`
+
   return (
     <header>
       <h1>Kitsu</h1>
       <h2>
-        {(match.params.season || season()).toUpperCase()}
-        <strong>{match.params.year || year()}</strong>
+        {season || s()}
+        <strong>{year || y()}</strong>
       </h2>
     </header>
-  )
-}
-
-HeaderContent.propTypes = {
-  match: PropTypes.object
-}
-
-export function Header () {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/:year/:season' component={HeaderContent}/>
-        <Route component={HeaderContent}/>
-      </Switch>
-    </Router>
   )
 }
