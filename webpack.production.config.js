9e90aa75b8e0b78f463a1421820377adf2f77a62
James Harris <wopian@wopian.me>
2018-01-02 02:09:38 +0000

    feat: add progressive manifest

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,19 @@
 var webpack = require('webpack')
 var path = require('path')
+var chalk = require('chalk')
 var loaders = require('./webpack.loaders')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-var StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var ProgressBarPlugin = require('progress-bar-webpack-plugin')
+var ProgressiveManifest = require('webpack-pwa-manifest')
+var BundleSizePlugin = require('webpack-bundle-size-analyzer').WebpackBundleSizeAnalyzerPlugin
 var { encode } = require('msgpack-lite/lib/encode')
-var { readFileSync, writeFileSync } = require('fs')
+var { readFileSync } = require('fs')
+
 
 loaders.push({
   test: /\.scss$/,
@@ -24,7 +29,8 @@ module.exports = {
   output: {
     publicPath: '/',
     path: path.join(__dirname, 'public'),
-    filename: '[chunkhash].js'
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -33,34 +39,99 @@ module.exports = {
     loaders
   },
   plugins: [
-    new WebpackCleanupPlugin(),
+    new ProgressBarPlugin({
+      format: '  ' + chalk.green.bold(':percent') + ' :elapseds :msg',
+      renderThrottle: 10
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        drop_console: true,
-        drop_debugger: true
-      }
+    new UglifyJsPlugin({
+      parallel: true,
+      cache: true
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new ExtractTextPlugin({
-      filename: '[chunkhash].css',
-      allChunks: true
+      filename: '[name].[contenthash].css',
+    }),
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        autoprefixer: true,
+        rawCache: true,
+        calc: true,
+        colormin: true,
+        convertValues: true,
+        discardComments: true,
+        discardDuplicates: true,
+        discardEmpty: true,
+        discardOverridden: true,
+        discardUnused: true, // May be unsafe
+        mergeIdents: true,
+        mergeLonghand: true,
+        mergeRules: true,
+        minifyFontValues: true,
+        minifyGradients: true,
+        minifyParams: true,
+        minifySelectors: true,
+        normalizeCharset: true,
+        normalizeDisplayValues: true,
+        normalizePositions: true,
+        normalizeRepeatStyle: true,
+        normalizeString: {
+          preferredQuote: 'single'
+        },
+        normalizeTimingFunctions: true,
+        normalizeUnicode: true,
+        normalizeUrl: true,
+        normalizeWhitespace: true,
+        orderedValues: true,
+        reduceIdents: true, // May be unsafe
+        reduceInitial: true,
+        reduceTransforms: true,
+        svgo: true,
+        uniqueSelectors: true,
+        zindex: true // May be unsafe
+      }
     }),
     new HtmlWebpackPlugin({
       template: './src/template.html',
-      files: {
-        css: ['style.css'],
-        js: ['bundle.js'],
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+      chunksSortMode: 'dependency'
+    }),
+    // keep module.id stable when vender modules does not change
+    new webpack.HashedModuleIdsPlugin(),
+    /*
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      async: 'common',
+      minChunks: 1
+    }),
+    */
+    // split vendor js into its own file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: module => {
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, 'node_modules')
+          ) === 0
+        )
       }
     }),
-    new StyleExtHtmlWebpackPlugin({
-      minify: true
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor']
     }),
     /* Does not support Webpack 3 yet
     new FaviconsWebpackPlugin({
@@ -88,6 +159,25 @@ module.exports = {
         to: '.',
         ignore: ['.*']
       }
-    ])
+    ]),
+    new ProgressiveManifest({
+      name: 'Kitsu Season Trends',
+      short_name: 'Season Trends',
+      description: 'Daily rating trends of seasonal anime',
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#332532',
+      background_color: '#332532',
+      icons: [
+        {
+          src: path.resolve('src/favicon.png'),
+          sizes: [ 16, 32, 96, 128, 192, 256, 512 ]
+        }
+      ],
+      inject: true,
+      fingerprints: true,
+      ios: true
+    }),
+    new BundleSizePlugin('../.bundlesize.yml')
   ]
 }
