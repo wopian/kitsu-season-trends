8f2a3075e71fd9e2255a879d143e07d51d410d59
James Harris <wopian@wopian.me>
2018-01-06 02:38:25 +0000

    fix(cron): only skip leftovers with no ratings

--- cron/util/checkExists.js
+++ cron/util/checkExists.js
@@ -1,12 +1,17 @@
-import { getMean, setAnime, updateAnime, NOW } from './'
+import { getMean, setAnime, updateAnime, counters } from './'
 import { db } from './db'
+import { season, year } from '../../src/util'
 
 export async function checkExists (data) {
   try {
     const ratings = getMean(data.ratingFrequencies)
-    if (!db.get(`data.${data.id}`).value()) await setAnime(data.id, data, ratings)
-    else await updateAnime(data.id, data, ratings)
-    db.set('updated', NOW).write()
+    const isThisSeason = season(data.startDate) === season() && year(data.startDate) === year()
+    // Require at least 1 rating...or have started airing in the current season
+    if (ratings.mean > 0 || isThisSeason) {
+      if (!db.get(`data.${data.id}`).value()) await setAnime(data.id, data, ratings)
+      else await updateAnime(data.id, data, ratings)
+      counters.updated.push(data.canonicalTitle)
+    } else counters.skipped.push(data.canonicalTitle)
   } catch (E) {
     console.error(`Errored while checking if ${data.canonicalTitle} (${db.id}) was in database:`)
     throw E
