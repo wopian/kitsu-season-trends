8e61288bdeec9f3529228bb6bc322d1365038b53
James Harris <wopian@wopian.me>
2018-03-22 06:52:38 +0000

    fix: actually remove pruned anime

--- cron/modules/prune/index.js
+++ cron/modules/prune/index.js
@@ -1,8 +1,8 @@
-import { store, remove } from '../../util'
+import { store } from '../../util'
 
 export async function prune () {
-  store.willBePruned.forEach(async id => {
+  store.willBePruned.forEach(id => {
     store.count.removed.push(store.data.data.find(anime => ~~anime.i === ~~id).t)
-    store.data.data = store.data.data::remove(id)
+    store.data.data = store.data.data.filter(anime => ~~anime.i !== ~~id)
   })
 }
