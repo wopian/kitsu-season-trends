43901889dbb69f768d326d930020caf8777c5efe
James Harris <wopian@wopian.me>
2018-03-25 23:52:01 +0100

    chore: remove useless bower exclusion

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     test: /\.jsx?$/,
-    exclude: /(node_modules|bower_components|public\/)/,
+    exclude: /(node_modules|public\/)/,
     loader: "babel-loader"
   },
   {
@@ -11,37 +11,37 @@ module.exports = [
   },
   {
     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "file-loader"
   },
   {
     test: /\.(woff|woff2)$/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?prefix=font/&limit=5000"
   },
   {
     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?limit=10000&mimetype=application/octet-stream"
   },
   {
     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?limit=10000&mimetype=image/svg+xml"
   },
   {
     test: /\.gif/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?limit=10000&mimetype=image/gif"
   },
   {
     test: /\.jpg/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?limit=10000&mimetype=image/jpg"
   },
   {
     test: /\.png/,
-    exclude: /(node_modules|bower_components)/,
+    exclude: /node_modules/,
     loader: "url-loader?limit=10000&mimetype=image/png"
   }
 ];
