b9532e4fc2601de417c9d3a7afc3bffd15b3a1b7
James Harris <wopian@wopian.me>
2018-03-25 22:59:47 +0100

    chore: make use of travis log folding for stat output

--- cron/modules/stats/index.js
+++ cron/modules/stats/index.js
@@ -1,11 +1,19 @@
 import { store } from '../../util'
+import chalk from 'chalk'
+
+const log = console.log
 
 export async function stats () {
   await Promise.all(Object.keys(store.count).map(async counter => {
     const count = store.count[counter].length
-    if (count > 0) console.log(`\n${counter[0].toUpperCase()}${counter.slice(1)}: ${count}\n`)
+    const counterColour = counter === 'added' ? 'greenBright' : counter === 'updated' ? 'blueBright' : 'redBright'
+    const counterLabel = counter.toUpperCase()
+
+    if (process.env.TRAVIS) log(`travis_fold:start:${counter}`)
+    if (count > 0) log(chalk`{bold.${counterColour} ${counterLabel}} ${count} anime`)
     await store.count[counter].sort().map(anime => {
-      console.log(`  ${anime}`)
+      log(chalk`  {gray ${anime}}`)
     })
+    if (process.env.TRAVIS) log(`travis_fold:end:${counter}`)
   }))
 }
