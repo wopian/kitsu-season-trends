c484e18bb572a1d4224a4d787c5120a629058082
James Harris <wopian@wopian.me>
2018-03-29 03:26:07 +0100

    refactor: prune all ids at once instead of mapping over array

--- cron/modules/prune/index.js
+++ cron/modules/prune/index.js
@@ -1,8 +1,10 @@
 import { store } from '../../util'
 
 export async function prune () {
-  await Promise.all(store.willBePruned.map(async id => {
-    store.count.removed.push(store.data.data.find(anime => anime.i === id).t)
-    store.data.data = await store.data.data.filter(anime => anime.i !== id)
-  }))
+  const ids = store.willBePruned
+  store.data.data = await store.data.data.filter(anime => {
+    const remove = ids.includes(anime.i)
+    if (remove) store.count.removed.push(anime.t)
+    return !remove
+  })
 }
