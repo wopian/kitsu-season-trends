eaad4db7d186d435eb895eb0b896993c0195c76b
James Harris <wopian@wopian.me>
2018-09-26 17:44:32 +0100

    fix(cron): call cron command with environment variables

--- cron/util/api/index.mjs
+++ cron/util/api/index.mjs
@@ -1,5 +1,6 @@
 import Kitsu from 'kitsu'
 import OAuth2 from 'client-oauth2'
+import { log } from '../log'
 
 export const api = new Kitsu();
 
@@ -10,7 +11,9 @@ const { owner } = new OAuth2({
 })
 
 if (process.env.KITSU_USERNAME) {
+  if (!process.env.KITSU_PASSWORD) throw 'Missing KITSU_PASSWORD environment variable';
   (async () => {
     api.headers['Authorization'] = `Bearer ${ (await owner.getToken(process.env.KITSU_USERNAME, process.env.KITSU_PASSWORD)).accessToken }`
+    log('Authenticated')
   })()
-}
+} else throw 'Not authenticated! Missing KITSU_USERNAME and KITSU_PASSWORD environment variables'
