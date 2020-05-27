45a29def75a80604ba7490b1cce1a4f7c1b38fcc
James Harris <wopian@wopian.me>
2020-05-27 10:29:51 +0100

    migrate: delete 2017 entries that had days with 0 ratings

--- cron/migrate.js
+++ cron/migrate.js
@@ -13,22 +13,24 @@ readdir(dir, (err, files) => {
       if (err2) throw err2
 
       const data = JSON5.parse(DATA)
+      const entriesToDelete = []
 
       data.data.forEach(entry => {
-        // Add n to 2017 data (set all as NEW)
-        //if (typeof entry.n === 'undefined') entry.n = 1
-
-        // Add u to 2017 data (set all as TV)
-        //if (typeof entry.u === 'undefined') entry.u = 0
-
         entry.d = entry.d.filter(day => typeof day.m !== 'undefined')
 
-        entry.d.forEach(day => {
-          if (typeof day.m === 'undefined') console.log(day)
+        entry.d.forEach((day, index) => {
+          if (index !== day.i) {
+            entriesToDelete.push(entry.i)
+          }
         })
 
 
       })
+
+      const deDupe = [...new Set(entriesToDelete)]
+      console.log(deDupe)
+
+      data.data = data.data.filter(entry => !deDupe.includes(entry.i))
 
       // Removed entries with less than 5 user ratings
       // data.data = data.data.filter(anime => anime.d[anime.d.length - 1].r >= 5)
