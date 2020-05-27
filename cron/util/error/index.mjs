47dd6dff3dceaafcea11dac96b6d35f787693c01
James Harris <wopian@wopian.me>
2020-05-27 04:29:13 +0100

    refactor: cleanup kitsu-core import

--- cron/util/error/index.mjs
+++ cron/util/error/index.mjs
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import { query } from 'kitsu-core/lib/index.mjs'
+import { query } from 'kitsu-core'
 import { log } from '../index.mjs'
 
 export function error (message, { url, params }) {
